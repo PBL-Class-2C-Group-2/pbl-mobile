ef51cd3c087f4d7c8034ca98149c236080aa4d30
dikanet8 <dikanet8@gmail.com>
2022-12-20 04:02:32 +0700

    PBL-MOBILE

--- lib/view/home.view.dart
+++ lib/view/home.view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_pbl/view/navbar.dart';
 
 class HomeView extends StatelessWidget {
@@ -9,64 +8,127 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromARGB(235, 191, 216, 250),
         foregroundColor: Colors.black,
       ),
       drawer: Sidebar(),
-      body: Column(
-        children: [
-          Container(
-            alignment: Alignment.topLeft,
-            padding: EdgeInsets.all(10),
-            child: Text(
-              'Selamat Datang di Desa Tambong',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Itim',
-                fontSize: 30,
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  './assets/home.jpg',
+                ),
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          Container(
-            padding: EdgeInsets.all(10),
             alignment: Alignment.topLeft,
-            child: Text(
-              'Telusuri keindahan alam desa dalam genggaman',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontFamily: 'Itim',
-                fontSize: 15,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            child: Container(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              width: double.infinity,
-              height: 60,
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.search,
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'Selamat Datang di',
+                  style: TextStyle(
+                    fontFamily: 'Itim',
+                    fontSize: 30,
+                  ),
+                ),
+                Text(
+                  'Desa Tambong',
+                  style: TextStyle(
+                    fontFamily: 'Itim',
+                    fontSize: 30,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Telusuri keindahan alam desa dalam genggaman',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontFamily: 'Itim',
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    width: double.infinity,
+                    height: 50,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Telusuri',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontFamily: 'Itim',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 255, 255, 255),
+                    shape: StadiumBorder(),
+                  ),
+                ),
+                SizedBox(height: 25),
+                Text(
+                  'Terbaru dari Tambong',
+                  style: TextStyle(
+                      fontFamily: 'Itim',
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                      fontSize: 15),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Image.asset(
+                      './assets/news/berita1.jpg',
+                    ),
+                    SizedBox(width: 20),
+                    Image.asset(
+                      './assets/news/berita2.png',
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Produk Lokal Tambong',
+                  style: TextStyle(
+                    fontFamily: 'Itim',
                     color: Colors.black,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 15,
                   ),
-                  SizedBox(width: 10),
-                  Text(
-                    'Telusuri',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
-              ),
-            ),
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              primary: Color.fromARGB(255, 255, 255, 255),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Image.asset(
+                      './assets/news/berita3.png',
+                    ),
+                    SizedBox(width: 20),
+                    Image.asset(
+                      './assets/news/berita4.png',
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
-    ;
   }
 }
