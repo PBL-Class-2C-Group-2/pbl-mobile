41262834410f7697a9f4cb79e219b0f9dba2531e
dikanet8 <dikanet8@gmail.com>
2022-12-19 15:15:01 +0700

    PBL-MOBILE

--- lib/view/home.view.dart
+++ lib/view/home.view.dart
@@ -13,13 +13,55 @@ class HomeView extends StatelessWidget {
       ),
       drawer: Sidebar(),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text(
-            'Selamat Datang di Desa Tambong',
-            style: TextStyle(
-              fontFamily: 'Itim',
-              fontSize: 30,
+          Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(10),
+            child: Text(
+              'Selamat Datang di Desa Tambong',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Itim',
+                fontSize: 30,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.topLeft,
+            child: Text(
+              'Telusuri keindahan alam desa dalam genggaman',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontFamily: 'Itim',
+                fontSize: 15,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 15),
+              width: double.infinity,
+              height: 60,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Telusuri',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              primary: Color.fromARGB(255, 255, 255, 255),
             ),
           ),
         ],
